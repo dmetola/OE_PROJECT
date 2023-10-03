3e2605333e8100b741f084cb18c8988326a2224a
Dario Metola Rodriguez <dametola@gmail.com>
2023-10-03 15:10:40 +0100

    Initial commit

--- oe_modules.py
+++
@@ -1,8 +0,0 @@
-import stanza
-
-nlp = stanza.Pipeline(
-    lang='en', 
-    processors='tokenize, lemma', 
-    tokenizer_model_path='saved_models/tokenize/ang_test_tokenizer.pt',
-    lemmatizer_model_path='saved_models/lemma/ang_test_lemmatizer.pt'
-)
